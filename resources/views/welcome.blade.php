7c7dd6b63041c1430f4cab4c2498890067f29513
Jack Sparrow <nicolasmeireles1004@gmail.com>
2024-08-24 01:09:04 -0300

    :tada: Initial.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Casamento Merda Bet</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app">
+        <h1 style="color: black">Casamento Merda Bet</h1>
+    </div>
+</body>
+</html>
