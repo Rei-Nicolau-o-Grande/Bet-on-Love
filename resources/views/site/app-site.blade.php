6d1b139afbf3ca17a5adadac7aea621ae438d36d
Jack Sparrow <nicolasmeireles1004@gmail.com>
2024-08-26 17:07:56 -0300

    :poop: feat: add navbar in package view site.

---
+++ resources/views/site/app-site.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Casamento Merda Bet</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+
+</head>
+<body>
+
+    <div id="app-site" class="w-full">
+
+        <header class="relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-800">
+            <x-site.navbar />
+        </header>
+
+        <section>
+
+        </section>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\Admin\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('site.app-site');
+})->name('home');
 
 Route::group(['prefix' => 'admin', 'middleware' => []], function () {
     Route::resources([
